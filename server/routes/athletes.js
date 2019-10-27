d6365bf609c62a1a1e229765198b74746bb0f99d
Dennis Xiao <dennisboys@gmail.com>
2019-10-27 16:40:14 +1000

    code clean up

--- server/routes/athletes.js
+++ server/routes/athletes.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const router = express.Router()
-const ATHLETE_DATA = require('../data/ATHLETE_DATA')
+const express = require('express');
+const router = express.Router();
+const ATHLETE_DATA = require('../data/ATHLETE_DATA');
 
 router.get('/athletes', (req, res) => {
   res.json(ATHLETE_DATA);
-})
+});
 
-module.exports = router
+module.exports = router;
