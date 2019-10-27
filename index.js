39cc0c3dc0f4d71817063531e0c3201169ec1d03
Dennis Xiao <dennisboys@gmail.com>
2019-10-27 16:44:25 +1000

    update server

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ app.use('/api', athletes);
 // The "catchall" handler: for any request that doesn't
 // match the above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+	res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
 
-app.listen(port);
+app.listen(port, error => {
+	if (error) throw error;
 
-console.log(`Server is listening on ${port}`);
+	console.log(`Server is listening on port ${port}!`);
+});
